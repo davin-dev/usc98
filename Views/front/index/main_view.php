5e9611637076038cb61e65e82d1daddccb0004bc
A <sweetwow0098@gmail.com>
2020-01-21 20:22:44 +0330

    slider changes

--- Views/front/index/main_view.php
+++ Views/front/index/main_view.php
@@ -1,12 +1,12 @@
 <script>
-    var slideIndex = 1;
+    var slideIndex = 0;
     showSlides(slideIndex);
 
     // Next/previous controls
     function plusSlides(n) {
     showSlides(slideIndex += n);
     }
-
+    
     // Thumbnail image controls
     function currentSlide(n) {
     showSlides(slideIndex = n);
@@ -19,7 +19,7 @@
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].style.display = "block";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
@@ -34,6 +34,7 @@
     function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
+    
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
@@ -68,6 +69,15 @@
   <!-- Next and previous buttons -->
   <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
   <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+  <!-- The dots/circles -->
+<div style="text-align:center">
+<?php $i = 1; foreach($news as $ns) { ?>
+  <span class="dot" onclick="currentSlide(<?= $i ?>)"></span>
+  <?php $i++;} // end of while ?>
+ 
+</div>
+
 </div>
 
 
--- Views/front/news/search_view.php
+++ Views/front/news/search_view.php
@@ -5,7 +5,7 @@
         <div class="category"><h3><?= $news["category"]; ?></h3></div>
         <div class="title"><h1><?= $news['headline'] ?></h1></div>
         <div class="pic"><img id="pic" src="<?= BASE_URL ?>public/img/<?= $news['picture'] ?>"/></div>
-        <div class="text"><?= $news['excerpt']; ?></div>
+        <div class="note"><?= $news['excerpt']; ?></div>
         <a class="continue" href="<?= BASE_URL ?>news/continued&id=<?php echo $news["id"]; ?>">Continue Reading</a>
     </div>
  <?php }else{ ?>
